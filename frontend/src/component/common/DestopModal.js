707e661d4a8c80995fca7dade047f82cedfff9e8
Deepanshu3130 <djjoshi3130@gmail.com>
2025-05-03 18:44:20 +0530

    final commit

--- frontend/src/component/common/DestopModal.js
+++ frontend/src/component/common/DestopModal.js
@@ -66,14 +66,14 @@ const DesktopModeModal = () => {
             </p>
           </div>
           <div className="mt-6 flex flex-col sm:flex-row gap-3">
-            <button
+            {/* <button
               type="button"
               onClick={openInDesktopMode}
               className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-richblack-900 bg-yellow-50 hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 transition-colors"
             >
               <FaDesktop className="mr-2" />
               Open in Desktop Mode
-            </button>
+            </button> */}
             <button
               type="button"
               onClick={closeModal}
