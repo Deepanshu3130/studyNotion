b0f5f70f930d10b9917bdd3af92aa3f11ff5ef8f
Deepanshu joshi <151924025+Deepanshu3130@users.noreply.github.com>
2026-05-16 02:01:22 +0530

    Fix CORS origin URL by removing trailing slash

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://studynotion1-c24e.onrender.com/",
+		origin:"https://studynotion1-c24e.onrender.com",
 		credentials:true,
 		maxAge: 14400,
 		allowedHeaders: ['Authorization', 'Content-Type'],
